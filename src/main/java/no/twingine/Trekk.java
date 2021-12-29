8a32f3122cac2d328adbb7ce33be7648a9256e20
Asgeir Storesund Nilsen <asgeir.nilsen@miles.no>
2021-12-29 11:43:38 +0100

    Google cloud function.

---
+++ src/main/java/no/twingine/Trekk.java
@@ -0,0 +1,25 @@
+package no.twingine;
+
+import java.io.PrintWriter;
+
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
+
+import no.skatteetaten.fastsetting.formueinntekt.forskudd.trekkrutine2022.Periode;
+import no.skatteetaten.fastsetting.formueinntekt.forskudd.trekkrutine2022.Tabellnummer;
+import no.skatteetaten.fastsetting.formueinntekt.forskudd.trekkrutine2022.Trekkrutine;
+
+public class Trekk implements HttpFunction {
+
+    @Override
+    public void service(HttpRequest req, HttpResponse res) throws Exception {
+        long amount = Long.parseLong(req.getFirstQueryParameter("amount").orElse("0"));
+        long trekk = Trekkrutine.beregnTabelltrekk(Tabellnummer.TABELL_7109, Periode.PERIODE_1_MAANED, amount);
+        res.setContentType("application/json");
+        try (PrintWriter pw = new PrintWriter(res.getWriter())) {
+            pw.printf("{ \"trekk\": %d }", trekk);
+        }
+    }
+
+}
