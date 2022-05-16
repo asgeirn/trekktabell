aa01b4a6b724df8c1b4cb86341abdc2b683e62a2
Asgeir Storesund Nilsen <asgeir@twingine.no>
2022-05-16 11:32:35 +0200

    Added Docker build, served using Undertow.

--- src/main/java/no/twingine/Trekk.java
+++ src/main/java/no/twingine/Trekk.java
@@ -2,25 +2,33 @@ package no.twingine;
 
 import java.io.PrintWriter;
 import java.math.BigDecimal;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
-import com.google.cloud.functions.HttpFunction;
-import com.google.cloud.functions.HttpRequest;
-import com.google.cloud.functions.HttpResponse;
-
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
 import no.skatteetaten.fastsetting.formueinntekt.forskudd.trekkrutine2022.Periode;
 import no.skatteetaten.fastsetting.formueinntekt.forskudd.trekkrutine2022.Tabellnummer;
 import no.skatteetaten.fastsetting.formueinntekt.forskudd.trekkrutine2022.Trekkrutine;
 
-public class Trekk implements HttpFunction {
+public class Trekk {
 
-    @Override
-    public void service(HttpRequest req, HttpResponse res) throws Exception {
-        BigDecimal amount = new BigDecimal(req.getFirstQueryParameter("amount").orElse("0"));
-        long trekk = Trekkrutine.beregnTabelltrekk(Tabellnummer.TABELL_7109, Periode.PERIODE_1_MAANED, amount.longValue());
-        res.setContentType("text/plain");
-        try (PrintWriter pw = new PrintWriter(res.getWriter())) {
-            pw.println(trekk);
-        }
+    public static void main(String[] args) throws UnknownHostException {
+        Undertow server = Undertow.builder()
+                .addHttpListener(8080, InetAddress.getLocalHost().getHostAddress())
+                .setHandler(new HttpHandler() {
+                    @Override
+                    public void handleRequest(HttpServerExchange exchange) throws Exception {
+                        BigDecimal amount = new BigDecimal(exchange.getQueryParameters().get("amount").element());
+                        long trekk = Trekkrutine.beregnTabelltrekk(Tabellnummer.TABELL_7109, Periode.PERIODE_1_MAANED,
+                                amount.longValue());
+                        exchange.getResponseHeaders().add(Headers.CONTENT_TYPE, "text/plain");
+                        exchange.getResponseSender().send(Long.toString(trekk));
+                    }
+                }).build();
+        server.start();
     }
 
 }
