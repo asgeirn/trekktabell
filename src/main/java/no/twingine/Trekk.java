8d983afb7004593cba967a1773e731084e618480
Asgeir Nilsen <asgeir@twingine.no>
2024-01-01 11:15:18 +0100

    Updated imports for 2024.

--- src/main/java/no/twingine/Trekk.java
+++ src/main/java/no/twingine/Trekk.java
@@ -9,9 +9,9 @@ import io.undertow.Undertow;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
-import no.skatteetaten.fastsetting.formueinntekt.forskudd.trekkrutine2023.Periode;
-import no.skatteetaten.fastsetting.formueinntekt.forskudd.trekkrutine2023.Tabellnummer;
-import no.skatteetaten.fastsetting.formueinntekt.forskudd.trekkrutine2023.Trekkrutine;
+import no.skatteetaten.fastsetting.formueinntekt.forskudd.trekkrutine2024.Periode;
+import no.skatteetaten.fastsetting.formueinntekt.forskudd.trekkrutine2024.Tabellnummer;
+import no.skatteetaten.fastsetting.formueinntekt.forskudd.trekkrutine2024.Trekkrutine;
 
 public class Trekk {
 
